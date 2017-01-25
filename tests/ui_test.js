ebace157856ba8312ab33c2721e8df1386204294
rubyconn <regen@anche.no>
2017-01-25 13:13:43 +0100

    Populated session objects for testing UI.

--- tests/ui_test.js
+++ tests/ui_test.js
@@ -1,5 +1,66 @@
 var logger = require('../lib/logger');
 
-var base = require('../lib/ui/base');
+var session = require('../lib/app')
+  , base = require('../lib/ui/base');
 
 logger.transports.customLogger.level = 'debug';
+
+session.users = {
+    '92837105728491852': { 
+        persona_state: "offline", 
+        player_name: "Gabe Newell", 
+        game_name: ""
+    },
+    '90864320871438724': {
+        persona_state: "ingame", 
+        player_name: "Robin Walker", 
+        game_name: "Age of Empires II HD Edition"
+    },
+    '93087430280872349': {
+        persona_state: "online", 
+        player_name: "IceFrog", 
+        game_name: ""
+    },
+    '92934987123508759': {
+        persona_state: "online", 
+        player_name: "The Pink Cookie",
+        game_name: ""
+    },
+    '93402347985239844': {
+        persona_state: "ingame", 
+        player_name: "Axel Gembe",
+        game_name: "Board Game Simulator"
+    },
+};
+
+session.friends = [ 
+    '92837105728491852', 
+    '90864320871438724',
+    '93087430280872349'
+];
+
+session.clans = {
+    '8294721957374875': {
+        clan_name: "Steam Chat",
+        user_count: 5,
+        user_list: 5,
+        members: {
+            '92837105728491852': "owner",
+            '90864320871438724': "officer",
+            '93087430280872349': "moderator",
+            '92934987123508759': "member",
+            '93402347985239844': "nobody"
+        }
+    }
+};
+
+
+var userwin = require('../lib/ui/userwin');
+
+setTimeout(() => {
+    userwin.updateFriend();
+}, 500);
+
+setTimeout(() => {
+    userwin.updateGroup('8294721957374875');
+}, 5000);
